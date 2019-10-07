5f97b5052cba37c6d032f07adc28c32396623659
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-10-06 21:20:47 -0300

    Inyectando mediante la interface

--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -3,8 +3,9 @@ package com.bolsadeideas.springboot.di.app.models.service;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MiServicio {
+public class MiServicio implements IServicio{
 	
+	@Override
 	public String operacion(){
 		return "ejecutando algún proceso importante";
 	}
