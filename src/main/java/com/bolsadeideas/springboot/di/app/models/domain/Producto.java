a650b7333dd3cdce8c3fc85a21424b03ee670926
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-06-06 12:24:14 -0300

    Registrando componente ItemsFactura

--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
@@ -6,6 +6,12 @@ public class Producto {
 	
 	private Integer precio;
 
+	public Producto(String nombre, Integer precio) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
